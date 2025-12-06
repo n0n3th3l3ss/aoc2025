469438d59e5166c08752cc8180603eeff0c8a324
n0n3th3l3ss <none@none.com>
2025-12-06 13:32:43 +0100

    Day 6 part II

--- src/main/scala/aoc/_6/_6_InputParser.scala
+++ src/main/scala/aoc/_6/_6_InputParser.scala
@@ -21,5 +21,43 @@ object _6_InputParser {
     numbers.transpose.zip(operations)
   }
 
+  def parseWithCaphalopodLogic(
+      lines: Seq[String]
+  ): Vector[(Vector[Long], Operation)] = {
+    val longestLine = lines.map(_.length).max
+
+    val numbers = lines.init
+      .map { line =>
+        val lineSplitToChars = line.chars().mapToObj(_.toChar).toArray.toVector
+        val diff = longestLine - lineSplitToChars.length
+        if diff > 0 then lineSplitToChars :++ Vector.fill(diff)(' ')
+        else lineSplitToChars
+      }
+      .transpose
+      .map(_.mkString)
+      .map(_.trim)
+      .map(_.toLongOption)
+      .foldLeft(Vector.empty[Vector[Long]]) { case (acc, num) =>
+        num match {
+          case Some(value) =>
+            acc match {
+              case xs :+ x => xs :+ (x :+ value)
+              case _       => Vector(Vector(value))
+            }
+          case None => acc :+ Vector.empty
+        }
+      }
+
+    val operations = lines.lastOption
+      .map { line => line.split("\\s++").filter(!_.isBlank).toVector }
+      .toVector
+      .flatten
+      .map {
+        case "+" => Add
+        case "*" => Mul
+      }
+
+    numbers.zip(operations)
+  }
 
 }
